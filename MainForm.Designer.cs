58c6ba31ff28fc1d1b72c8af6f4c77e821b66d33
Bliss <31372684+BlissSc@users.noreply.github.com>
2019-12-28 04:55:58 -0300

    pene
    pene

---
+++ MainForm.Designer.cs
@@ -0,0 +1,235 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Usuario
+ * Fecha: 28-12-2019
+ * Hora: 3:54
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+namespace jons2
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.SystemColors.Window;
+			this.label1.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(0, 52);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(630, 55);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "tu comPUTA dora a sido hjackeada OMG GGG";
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(0, 107);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(630, 40);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "para revertir el efecto jons(gay) pasa este test de jusgamiento";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(12, 165);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(91, 88);
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(13, 260);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(90, 23);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "eri gey?";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.Location = new System.Drawing.Point(128, 165);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(104, 24);
+			this.radioButton1.TabIndex = 4;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "ci";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1CheckedChanged);
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.Location = new System.Drawing.Point(128, 206);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(104, 24);
+			this.radioButton2.TabIndex = 5;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "non";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2CheckedChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(521, 206);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(153, 67);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "CLICK ACA PARA VER LAS MEJORES NALGONASSSS";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(128, 259);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(104, 23);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "ok jusgame";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(12, 383);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(602, 23);
+			this.progressBar1.TabIndex = 8;
+			this.progressBar1.Value = 50;
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(12, 325);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(602, 55);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "META DE SUSCRIPTORES GENTE";
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 2.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(722, 313);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(100, 10);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "pilupatu tay rika";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(449, 151);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(358, 38);
+			this.button3.TabIndex = 11;
+			this.button3.Text = "ATENCION PERU NO ES UN ESTADO LEGITIMO";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// label6
+			// 
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(573, 0);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(249, 10);
+			this.label6.TabIndex = 12;
+			this.label6.Text = "por ñliss XD no me hago responsable de niuna wea";
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(152, 22);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(100, 23);
+			this.label7.TabIndex = 13;
+			this.label7.Text = "JONS.EXE 2";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(819, 418);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "jons2";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,54 @@
+﻿// ESTA WEA ES PA DIVERTIRME
+using System;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace jons2
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			Process.Start("https://discord.gg/cbwPJw5");
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			if (radioButton1.Checked) { 
+				MessageBox.Show("GAY");
+				for (int x = 1; x <= 39; x++) Process.Start("https://google.com/search?q=SIMULADOR DE SEXO SIN TARJETA DE CREDITO");
+			}
+			if (radioButton2.Checked) MessageBox.Show("muy bien te pasaste crack.");
+			DialogResult result = MessageBox.Show("Deseas unirte a mi discord?",  "XD", MessageBoxButtons.YesNo);
+		     if (result == DialogResult.Yes){
+				Process.Start("https://discord.gg/cbwPJw5");
+				MessageBox.Show("Unete porfa :) gracias por ser buena gente");
+			  }
+			else if (result == DialogResult.No){
+				Process.Start("https://google.com/search?q=SIMULADOR DE SEXO SIN TARJETA DE CREDITO");
+				for (int x = 1; x <= 70; x++) MessageBox.Show("AZUNIX ES GAY | peru es un estado no legitimo y el tamaño promedio es de 0.5cm", "pudrete, son 145 o reincio");
+			}
+				
+		}
+		void RadioButton2CheckedChanged(object sender, EventArgs e)
+		{}
+		void RadioButton1CheckedChanged(object sender, EventArgs e)
+		{}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			MessageBox.Show("no usi esta wea si tus papis estan atras....");
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			Process.Start("https://google.cl/search?q=Porno de Minecraft");
+		}
+		
+	}
+}
